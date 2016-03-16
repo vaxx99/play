92fd076cd2ecf7407677006401a745c9fabf88d8
vaxx99 <vaxx99@gmail.com>
2016-03-16 10:20:38 +0200

    Update red

---
+++ wapi.js
@@ -0,0 +1,64 @@
+var context = new AudioContext();
+  var mediaElement = document.getElementById('al');
+  var sourceNode = context.createMediaElementSource(mediaElement);
+
+  // EQ Properties
+  //
+  var gainDb = -40.0;
+  var bandSplit = [360,3600];
+
+  var hBand = context.createBiquadFilter();
+  hBand.type = "lowshelf";
+  hBand.frequency.value = bandSplit[0];
+  hBand.gain.value = gainDb;
+
+  var hInvert = context.createGain();
+  hInvert.gain.value = -1.0;
+
+  var mBand = context.createGain();
+
+  var lBand = context.createBiquadFilter();
+  lBand.type = "highshelf";
+  lBand.frequency.value = bandSplit[1];
+  lBand.gain.value = gainDb;
+
+  var lInvert = context.createGain();
+  lInvert.gain.value = -1.0;
+
+  sourceNode.connect(lBand);
+  sourceNode.connect(mBand);
+  sourceNode.connect(hBand);
+
+  hBand.connect(hInvert);
+  lBand.connect(lInvert);
+
+  hInvert.connect(mBand);
+  lInvert.connect(mBand);
+
+  var lGain = context.createGain();
+  var mGain = context.createGain();
+  var hGain = context.createGain();
+
+  lBand.connect(lGain);
+  mBand.connect(mGain);
+  hBand.connect(hGain);
+
+  var sum = context.createGain();
+  lGain.connect(sum);
+  mGain.connect(sum);
+  hGain.connect(sum);
+  sum.connect(context.destination);
+
+  // Input
+  //
+  function changeGain(string,type)
+  {
+    var value = parseFloat(string) / 100.0;
+    
+    switch(type)
+    {
+      case 'lowGain': lGain.gain.value = value; break;
+      case 'midGain': mGain.gain.value = value; break;
+      case 'highGain': hGain.gain.value = value; break;
+    }
+  }
